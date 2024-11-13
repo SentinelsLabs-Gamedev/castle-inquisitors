dff20488ea9759be78f2cc5231e475679ab0da95
Marino Gomez <37821609+DarthMarino@users.noreply.github.com>
2024-11-13 17:34:30 -0400

    Feat/base UI debugging (#9)
    * Feat: Added base gameplay debugger template.
    - Via "'" apostrophe we can open the Gameplay Debugger and see the player category options.
    - You are able to add more Player params, through the Debugger Folder, and you can add more categories reproducing the template.
    
    * feat: Add solution file for ease of use.

--- Source/CastleInquisitors/CastleInquisitors.cpp
+++ Source/CastleInquisitors/CastleInquisitors.cpp
@@ -3,5 +3,41 @@
 #include "CastleInquisitors.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CastleInquisitors, "CastleInquisitors" );
- 
+#if WITH_GAMEPLAY_DEBUGGER
+	#include "GameplayDebugger.h"
+	#include "Debugger/CI_FGameplayDebuggerCategory_Player.h"
+#endif
+
+DEFINE_LOG_CATEGORY_STATIC(LogCastleInquisitors, Log, All);
+
+class FCastleInquisitorsModule : public FDefaultGameModuleImpl
+{
+public:
+	virtual void StartupModule() override
+	{
+		UE_LOG(LogCastleInquisitors, Log, TEXT("FCastleInquisitorsModule has started up"));
+		#if WITH_GAMEPLAY_DEBUGGER
+		UE_LOG(LogCastleInquisitors, Log, TEXT("Registering Gameplay Debugger Category"));
+		IGameplayDebugger& GameplayDebuggerModule = IGameplayDebugger::Get();
+		GameplayDebuggerModule.RegisterCategory(
+			"Player",
+			IGameplayDebugger::FOnGetCategory::CreateStatic(&CI_FGameplayDebuggerCategory_Player::MakeInstance),
+			EGameplayDebuggerCategoryState::EnabledInGameAndSimulate,
+			6
+		);
+		GameplayDebuggerModule.NotifyCategoriesChanged();
+		#endif
+	}
+
+	virtual void ShutdownModule() override
+	{
+		UE_LOG(LogCastleInquisitors, Log, TEXT("FCastleInquisitorsModule is shutting down"));
+		#if WITH_GAMEPLAY_DEBUGGER
+		IGameplayDebugger& GameplayDebuggerModule = IGameplayDebugger::Get();
+		GameplayDebuggerModule.UnregisterCategory("Player");
+		GameplayDebuggerModule.NotifyCategoriesChanged();
+		#endif // WITH_GAMEPLAY_DEBUGGER
+	}
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FCastleInquisitorsModule, CastleInquisitors, "CastleInquisitors");
